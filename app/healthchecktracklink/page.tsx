227ddf747d42f6647f3dfe29a8886a8fdc930ed5
WurfelSPA <wurfel.cl@gmail.com>
2026-06-04 11:52:35 -0400

    Remove 'Instalaciones' button from healthcheck page

--- app/healthchecktracklink/page.tsx
+++ app/healthchecktracklink/page.tsx
@@ -201,7 +201,6 @@ export default function HealthCheckTracklink() {
   <div className="flex gap-6 font-semibold flex-1">
     <button onClick={() => router.push("/")} className="hover:text-yellow-300">Home</button>
     <button onClick={() => router.push("/renovaciones")} className="hover:text-yellow-300">Renovaciones</button>
-    <button onClick={() => router.push("/")} className="hover:text-yellow-300">Instalaciones</button>
     <button className="text-yellow-300 underline">Tracklink Healthcheck</button>
     <button onClick={() => router.push("/healthcheckmazda")} className="hover:text-yellow-300">Mazda Healthcheck</button>
   </div>
